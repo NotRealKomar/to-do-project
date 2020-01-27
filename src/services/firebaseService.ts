8d72f8a868ff2283414c8bc86c7174f9781de9b3
Vladislav Komar <v.komar@itransition.com>
2020-01-27 12:52:10 +0300

    Exclude config variables

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -1,18 +1,9 @@
 import firebase from 'firebase/app';
+import config from '../config';
 import "firebase/auth";
 import "firebase/database";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCi5m6hYjh00Bg3aHbQ49aFFCt4En45ZpI",
-    authDomain: "to-do-f4da2.firebaseapp.com",
-    databaseURL: "https://to-do-f4da2.firebaseio.com",
-    projectId: "to-do-f4da2",
-    storageBucket: "to-do-f4da2.appspot.com",
-    messagingSenderId: "459079348233",
-    appId: "1:459079348233:web:09620da831b56af08488d8",
-};
-
-export const firebaseService = firebase.initializeApp(firebaseConfig);
+export const firebaseService = firebase.initializeApp(config);
 const databaseRef = firebase.database().ref();
 
 export const toDosRef = databaseRef.child("toDos");
