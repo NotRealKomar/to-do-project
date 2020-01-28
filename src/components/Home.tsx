3a81962ca711a9e9457e572608041e5a090b56da
Vladislav Komar <v.komar@itransition.com>
2020-01-28 18:44:40 +0300

    Update protected route

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions/loginActions";
 import { LoginState } from "../reducers/loginReducer";
