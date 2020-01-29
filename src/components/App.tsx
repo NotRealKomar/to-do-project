e056b61fff81ff509c1607812a378e698e652539
Vladislav Komar <v.komar@itransition.com>
2020-01-29 10:00:57 +0300

    Update components code style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { LoginState } from '../reducers/loginReducer'
+import { LoginState } from '../reducers/loginReducer';
 import { Switch, Route } from 'react-router';
 import { verifyAuth } from '../actions/loginActions';
 
@@ -9,35 +9,36 @@ import Home from './Home';
 import Login from './login/Login';
 
 interface IProps {
-  isAuthenticated: boolean;
-  isVerifying: boolean;
-  verifyAuth: () => void;
+	isAuthenticated: boolean;
+	isVerifying: boolean;
+	verifyAuth: () => void;
 }
 
 const App: React.FC<IProps> = (props) => {
-    const { isAuthenticated, isVerifying, verifyAuth } = props;
-    useEffect(() => {
-      verifyAuth();
-    }, [ verifyAuth ]);
+	const { isAuthenticated, isVerifying, verifyAuth } = props;
+	useEffect(() => {
+		verifyAuth();
+	}, [ verifyAuth ]);
 
-    return (
-      <Switch>
-        <ProtectedRoute
-          exact
-          path="/"
-          component={Home}
-          isAuthenticated={isAuthenticated}
-          isVerifying={isVerifying}
-        />
-        <Route path="/login" component={Login} />
-      </Switch>
-    );
-  }
+	return (
+		<Switch>
+			<ProtectedRoute
+				exact
+				path="/"
+				component={Home}
+				isAuthenticated={isAuthenticated}
+				isVerifying={isVerifying}
+			/>
+			<Route path="/login" component={Login} />
+		</Switch>
+	);
+};
+
 const mapStateToProps = (state: LoginState) => (
-  {
-    isAuthenticated: state.login.isAuthenticated,
-    isVerifying: state.login.isVerifying
-  }
-)
+	{
+		isAuthenticated: state.login.isAuthenticated,
+		isVerifying: state.login.isVerifying
+	}
+);
 
 export default connect(mapStateToProps, { verifyAuth })(App);
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,38 +1,39 @@
-import React from "react";
-import { connect } from "react-redux";
-import { logoutUser } from "../actions/loginActions";
-import { LoginState } from "../reducers/loginReducer";
+import React from 'react';
+import { connect } from 'react-redux';
+import { logoutUser } from '../actions/loginActions';
+import { LoginState } from '../reducers/loginReducer';
 
-import List from "./toDo/List";
+import List from './toDo/List';
 
 interface IProps {
-  logoutUser: () => void;
-  isLoggingOut: boolean;
-  logoutError: boolean;
+	logoutUser: () => void;
+	isLoggingOut: boolean;
+	logoutError: boolean;
 }
 
 const Home: React.FC<IProps> = (props) => {
-  const { isLoggingOut, logoutError, logoutUser } = props;
+	const { isLoggingOut, logoutError, logoutUser } = props;
 
-  const handleLogout = () => {
-    logoutUser();
-  };
+	const handleLogout = () => {
+		logoutUser();
+	};
 
-  return (
-    <div>
-      <List />
-      
-      <button onClick={handleLogout}>Logout</button>
-      {isLoggingOut && <p>Logging Out....</p>}
-      {logoutError && <p>Error logging out</p>}
-    </div>
-  );
-}
+	return (
+		<div>
+			<List />
+			
+			<button onClick={handleLogout}>Logout</button>
+			{isLoggingOut && <p>Logging Out....</p>}
+			{logoutError && <p>Error logging out</p>}
+		</div>
+	);
+};
+
 const mapStateToProps = (state: LoginState) => (
-  {
-    isLoggingOut: state.login.isLoggingOut,
-    logoutError: state.login.logoutError
-  }
-)
+	{
+		isLoggingOut: state.login.isLoggingOut,
+		logoutError: state.login.logoutError
+	}
+);
 
 export default connect(mapStateToProps, { logoutUser })(Home);
