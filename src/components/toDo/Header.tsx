f275b09ac6eb21bf190dd432574fef4bb3392743
Vladislav Komar <v.komar@itransition.com>
2020-02-03 12:54:04 +0300

    Move logout button to header component

--- src/components/toDo/Header.tsx
+++ src/components/toDo/Header.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import '../../styles/header.scss';
 import '../../styles/fontawesome/fontawesome.scss';
+import { Dispatch, ActionCreator, bindActionCreators } from 'redux';
+import * as ActionCreators from '../../actions/loginActions';
+import { IAction } from '../../actions/loginActions';
+import { connect } from 'react-redux';
+import { LoginState } from '../../reducers/loginReducer';
 
-const Header: React.FC = () => (
-	<header className="header header_light">
-		<h4 className="header__content">
-				[<i className="fas fa-clipboard-list"></i>]
-				To-Dooooooooo...ooo
-		</h4>
-	</header>
+interface IDispatchProps {
+	logoutUser: () => void;
+}
+
+interface IStateProps {
+	isLoggingOut: boolean;
+	logoutError: boolean;
+}
+
+type Props = IDispatchProps & IStateProps
+
+const Header: React.FC<Props> = (props) => {
+	const { logoutUser } = props;
+
+	return (
+		<header className="header header_light">
+			<h4 className="header__content">
+					[<i className="fas fa-clipboard-list"></i>]
+					To-Dooooooooo...ooo
+			</h4>
+
+			<button className="header__button-logout" onClick={logoutUser}>Logout</button>
+		</header>
+	);
+};
+
+const mapStateToProps: (state: LoginState) => IStateProps = (state) => (
+	{
+		isLoggingOut: state.login.isLoggingOut,
+		logoutError: state.login.logoutError
+	}
 );
 
-export default Header;
+const mapDispatchToProps: (dispatch: Dispatch, actions: ActionCreator<IAction>) => IDispatchProps = (dispatch, actions) => (
+	bindActionCreators(ActionCreators, dispatch)
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
