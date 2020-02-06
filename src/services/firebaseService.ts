25d2db6cf453d27ffd92fe949d0273f61fa39d44
Vladislav Komar <v.komar@itransition.com>
2020-02-06 12:34:17 +0300

    Update firebase service

--- src/services/firebaseService.ts
+++ src/services/firebaseService.ts
@@ -2,8 +2,11 @@ import firebase from 'firebase/app';
 import config from '../config';
 import 'firebase/auth';
 import 'firebase/database';
+ 
+export const firebaseService = (!firebase.apps.length) 
+  ? firebase.initializeApp(config) 
+  : firebase.app();
 
-export const firebaseService = firebase.initializeApp(config);
 const databaseRef = firebase.database().ref();
 
 export const toDosRef = databaseRef.child('toDos');
