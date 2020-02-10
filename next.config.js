8c1bb496077315e8dd5c459f6d5166b3ff6fe32e
Vladislav Komar <v.komar@itransition.com>
2020-02-10 10:21:03 +0300

    Update config and package.json

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /* eslint-disable no-undef */
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS(withSass({
+const withFonts = require('next-fonts');
+module.exports = withFonts(withCSS(withSass({
   webpack (config, options) {
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000
-        }
-      }
-    });
-
     return config;
   }
-}));
+})));
